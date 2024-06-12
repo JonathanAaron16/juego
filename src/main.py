c61bcbf63813f399deb063aa2268c9138273e828
JonathanAaron16 <joni.peri72@gmail.com>
2024-06-12 12:43:43 -0300

    comenzamos con el  pygame

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
 import pygame
+from setting import *
+
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Primer Juego")
+
+
+SCREEN.fill(YELLOW)
+
+contador = 0
+is_running = True
+
+while is_running:
+    print(contador)
+    contador += 1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+    pygame.display.flip()
+
+
+pygame.quit()
