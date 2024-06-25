c9fab5ec7dc2cf1251f0d11a019fd30f3d58b12f
JonathanAaron16 <122915483+JonathanAaron16@users.noreply.github.com>
2024-06-25 20:26:56 -0300

    Add files via upload
    colisiones

--- src/main_colision.py
+++ src/main_colision.py
@@ -13,10 +13,8 @@ direcciones = (DR,UR,DL,UL)
 
 block_width = 100
 block_height = 100
-count_coins = 25
+count_blocks = 2
 
-width_coin = 20
-height_coin = 20
 
 pygame.init()
 
@@ -50,12 +48,11 @@ def create_block(left = 0,top= 0,width= 50, height=50, color = (255,255,255 ),di
     return {"rect": pygame.Rect(left,top,width, height),"color": color,"dir": dire, "borde": borde, "radio": radio}
           
 
+blocks = []
 
-block = create_block(randint(0,WIDTH -block_width),randint(0,WIDTH -block_width),block_width,block_height,random_color(), direcciones[randrange(len(direcciones))])
+for block in range(count_blocks):
+    blocks.append(create_block(randint(0,WIDTH -block_width),randint(0,WIDTH -block_width),block_width,block_height,random_color(), direcciones[randrange(len(direcciones))]))
 
-coins = []
-for i in range(count_coins):
-    coins.append(create_block(randint(0,WIDTH -width_coin),randint(0,HEIGHT -height_coin),width_coin,height_coin,YELLOW,0,0,height_coin // 2))
 
 #comienzo random
 # blocks = [{"rect": pygame.Rect(randint(0,WIDTH -block_width),randint(0,WIDTH -block_width),block_width,block_height),"color": GREEN,"dir": UL, "borde": 0 , "radio": -1},
@@ -81,63 +78,58 @@ while is_running:
             is_running = False
 
     #actualizamos elemetos
-    
+    for block in blocks:
     #actualizamos direcion
-    if block["rect"].right >= WIDTH:
-        if block["dir"] == DR:
-            block["dir"] = DL
-        else:
-            block["dir"] = UL
-        block["color"] = get_random_color(colores)
+        if block["rect"].right >= WIDTH:
+            if block["dir"] == DR:
+                block["dir"] = DL
+            else:
+                block["dir"] = UL
+            block["color"] = get_random_color(colores)
 
-    elif block["rect"].left <= 0:
-        if block["dir"] == DL:
-            block["dir"] = DR
-        else:
-            block["dir"] = UR
-        block["color"] = random_color()
+        elif block["rect"].left <= 0:
+            if block["dir"] == DL:
+                block["dir"] = DR
+            else:
+                block["dir"] = UR
+            block["color"] = random_color()
+            
+        elif block["rect"].bottom >= HEIGHT:
+            if block["dir"] == DR:
+                block["dir"] = UR
+            else:
+                block["dir"] = UL
+            block["borde"] = randint(0,10)
+        elif block["rect"].top <= 0:
+            if block["dir"] == UR:
+                block["dir"] = DR
+            else:
+                block["dir"] = DL
+            block["radio"] = randint(-1,20)
         
-    elif block["rect"].bottom >= HEIGHT:
-        if block["dir"] == DR:
-            block["dir"] = UR
-        else:
-            block["dir"] = UL
-        block["borde"] = randint(0,10)
-    elif block["rect"].top <= 0:
-        if block["dir"] == UR:
-            block["dir"] = DR
-        else:
-            block["dir"] = DL
-        block["radio"] = randint(-1,20)
-    
         #actualizamos movimiento
-  
+    for block in blocks:
         
-    if block["dir"] == DR:
-        block["rect"].x += speed
-        block["rect"].y += speed
-    elif block["dir"] == DL:
-        block["rect"].x -= speed
-        block["rect"].y += speed       
-    elif block["dir"] == UL:
-        block["rect"].x -= speed
-        block["rect"].y -= speed 
-    elif block["dir"] == UR:
-        block["rect"].x += speed
-        block["rect"].y -= speed 
-
-    for coin in coins[:]:   
-        if dectetar_colision(coin["rect"],block["rect"]):
-            coins.remove(coin)
+        if block["dir"] == DR:
+            block["rect"].x += speed
+            block["rect"].y += speed
+        elif block["dir"] == DL:
+            block["rect"].x -= speed
+            block["rect"].y += speed       
+        elif block["dir"] == UL:
+            block["rect"].x -= speed
+            block["rect"].y -= speed 
+        elif block["dir"] == UR:
+            block["rect"].x += speed
+            block["rect"].y -= speed 
+        
+    if dectetar_colision(blocks[0]["rect"],blocks[1]["rect"]):
+        print("colision!!!")
 
     #dibujamos pantalla
     SCREEN.fill(CUSTOM) 
-    
-    pygame.draw.rect(SCREEN,block["color"],block["rect"],block["borde"],block["radio"])
-
-    for coin in coins:
-        pygame.draw.rect(SCREEN,coin["color"],coin["rect"],coin["borde"],coin["radio"])
-
+    for block in blocks:
+        pygame.draw.rect(SCREEN,block["color"],block["rect"],block["borde"],block["radio"])
 
 
 
